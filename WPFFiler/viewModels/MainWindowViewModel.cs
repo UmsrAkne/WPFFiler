2cd05ccc6e69edaafb795d219ed02c17fca47eb3
Akn <sibata486@gmail.com>
2020-08-11 18:00:26 +0900

    UI変更 / FileList をバインド済みの ListView を追加

--- WPFFiler/viewModels/MainWindowViewModel.cs
+++ WPFFiler/viewModels/MainWindowViewModel.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPFFiler.models;
 
 namespace WPFFiler.viewModels {
-    class MainWindowViewModel {
+    class MainWindowViewModel : BindableBase {
+        public FileList FileList {
+            get;
+            private set;
+        } = new FileList(@"C:\");
     }
 }
