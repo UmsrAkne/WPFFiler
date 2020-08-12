3aaee693a72d1588fbcfdc8a3a599e40c191568a
Akn <sibata486@gmail.com>
2020-08-12 12:46:39 +0900

    クラス追加 / ファイル、ディレクトリを表すモデルクラスを追加

---
+++ WPFFiler/models/ExFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFFiler.models {
+    public class ExFile {
+
+        private FileSystemInfo content;
+        public FileSystemInfo Content {
+            get => content;
+            set {
+                content = value;
+            }
+        }
+
+        private string currentPath;
+        public string CurrentPath {
+            get => currentPath;
+            set {
+                currentPath = value;
+            }
+        }
+
+        public ExFile(string path) {
+            FileSystemInfo f;
+            CurrentPath = path;
+            if (!Exists(path)) {
+                return;
+            }
+
+            Content = (Directory.Exists(path)) ? (FileSystemInfo)new DirectoryInfo(path) : (FileSystemInfo)new FileInfo(path);
+        }
+
+        public Boolean Exists(string path) {
+            return (File.Exists(path) || Directory.Exists(path));
+        }
+
+    }
+}
