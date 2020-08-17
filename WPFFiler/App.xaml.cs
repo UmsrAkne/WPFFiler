2bf778cc51677e4a46916a73d6182ed8dc6607d2
Akn <sibata486@gmail.com>
2020-08-17 13:37:11 +0900

    Appクラスの内容を変更

--- WPFFiler/App.xaml.cs
+++ WPFFiler/App.xaml.cs
@@ -5,11 +5,22 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Prism.Ioc;
+using WPFFiler.viewModels;
+using WPFFiler.views;
 
 namespace WPFFiler {
     /// <summary>
     /// App.xaml の相互作用ロジック
     /// </summary>
-    public partial class App : Application {
+    public partial class App {
+        protected override Window CreateShell() {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry) {
+            // ビューとビューモデルを登録
+            containerRegistry.RegisterDialog<InputDialog, InputDialogViewModel>();
+        }
     }
 }
