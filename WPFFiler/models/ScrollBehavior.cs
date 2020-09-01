f06d15ed46158b8d570b99939e8cfee9fa6b0d51
Akn <sibata486@gmail.com>
2020-09-02 02:34:49 +0900

    機能修正 / ScrollIntoView() を呼び出す behavior を listView にアタッチ

---
+++ WPFFiler/models/ScrollBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace WPFFiler.models
+{
+    class ScrollBehavior : Behavior<ListBox > {
+
+        protected override void OnAttached() {
+            base.OnAttached();
+            this.AssociatedObject.SelectionChanged += scrollView;
+        }
+
+        protected override void OnDetaching() {
+            base.OnDetaching();
+            this.AssociatedObject.SelectionChanged -= scrollView;
+        }
+
+        private void scrollView(object sender, EventArgs e) {
+            var lb = (ListBox)sender;
+            lb.ScrollIntoView(lb.SelectedItem);
+        }
+    }
+}
