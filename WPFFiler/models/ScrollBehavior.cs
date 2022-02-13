65741e17d208b1edad3fbde0ca76027c6767b044
Akn <sibata486@gmail.com>
2022-02-13 23:18:53 +0900

    refactor / 各ファイルを自動整形

--- WPFFiler/models/ScrollBehavior.cs
+++ WPFFiler/models/ScrollBehavior.cs
@@ -1,27 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using Microsoft.Xaml.Behaviors;
-
-namespace WPFFiler.models
+﻿namespace WPFFiler.models
 {
-    class ScrollBehavior : Behavior<ListBox > {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using Microsoft.Xaml.Behaviors;
 
-        protected override void OnAttached() {
+    public class ScrollBehavior : Behavior<ListBox>
+    {
+
+        protected override void OnAttached()
+        {
             base.OnAttached();
             this.AssociatedObject.SelectionChanged += scrollView;
         }
 
-        protected override void OnDetaching() {
+        protected override void OnDetaching()
+        {
             base.OnDetaching();
             this.AssociatedObject.SelectionChanged -= scrollView;
         }
 
-        private void scrollView(object sender, EventArgs e) {
+        private void scrollView(object sender, EventArgs e)
+        {
             var lb = (ListBox)sender;
             lb.ScrollIntoView(lb.SelectedItem);
         }
--- WPFFiler/viewModels/MainWindowViewModel.cs
+++ WPFFiler/viewModels/MainWindowViewModel.cs
@@ -1,20 +1,23 @@
-﻿using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WPFFiler.models;
-using Prism.Services.Dialogs;
-using Prism.Commands;
-using System.ComponentModel;
+﻿namespace WPFFiler.ViewModels
+{
+    using Prism.Mvvm;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using WPFFiler.models;
+    using Prism.Services.Dialogs;
+    using Prism.Commands;
+    using System.ComponentModel;
 
-namespace WPFFiler.ViewModels {
-    class MainWindowViewModel : BindableBase {
+    public class MainWindowViewModel : BindableBase
+    {
         private FileList fileList = new FileList(@"C:\");
-        public FileList FileList {
+        public FileList FileList
+        {
             get => fileList;
             private set => SetProperty(ref fileList, value);
         }
@@ -22,7 +25,8 @@ namespace WPFFiler.ViewModels {
         private FileList mainFileListStorage = null;
 
         private FileList subFileList = new FileList(@"C:\");
-        public FileList SubFileList {
+        public FileList SubFileList
+        {
             get => subFileList;
             private set => SetProperty(ref subFileList, value);
         }
@@ -30,15 +34,18 @@ namespace WPFFiler.ViewModels {
         private FileList subFileListStorage = null;
 
         private FileListControlCommands fileListControlCommands;
-        public FileListControlCommands FileListControlCommands {
+        public FileListControlCommands FileListControlCommands
+        {
             get => fileListControlCommands;
             set => SetProperty(ref fileListControlCommands, value);
         }
 
         private IDialogService dialogService;
 
-        public String CurrentDirectoriesPath {
-            get {
+        public String CurrentDirectoriesPath
+        {
+            get
+            {
                 DirectoryInfo di1 = new DirectoryInfo(FileList.CurrentDirectoryPath);
                 DirectoryInfo di2 = new DirectoryInfo(SubFileList.CurrentDirectoryPath);
                 return (FileList.BothViewBinding || SubFileList.BothViewBinding)
@@ -46,12 +53,15 @@ namespace WPFFiler.ViewModels {
             }
         }
 
-        public MainWindowViewModel(IDialogService dialogService) {
+        public MainWindowViewModel(IDialogService dialogService)
+        {
             this.dialogService = dialogService;
             FileListControlCommands = new FileListControlCommands(dialogService, FileList, SubFileList);
 
-            PropertyChangedEventHandler pcEventHandler = (Object sender, PropertyChangedEventArgs p) => {
-                if (p.PropertyName == nameof(FileList.CurrentDirectoryPath)){
+            PropertyChangedEventHandler pcEventHandler = (Object sender, PropertyChangedEventArgs p) =>
+            {
+                if (p.PropertyName == nameof(FileList.CurrentDirectoryPath))
+                {
                     RaisePropertyChanged(nameof(CurrentDirectoriesPath));
                 }
             };
@@ -61,10 +71,13 @@ namespace WPFFiler.ViewModels {
         }
 
         private DelegateCommand changeToMirrorModeCommand;
-        public DelegateCommand ChangeToMirrorModeCommand {
+        public DelegateCommand ChangeToMirrorModeCommand
+        {
             get => changeToMirrorModeCommand ?? (changeToMirrorModeCommand = new DelegateCommand(
-                () => {
-                    if(mainFileListStorage == null && subFileListStorage == null) {
+                () =>
+                {
+                    if (mainFileListStorage == null && subFileListStorage == null)
+                    {
                         mainFileListStorage = FileList;
                         subFileListStorage = SubFileList;
                         SubFileList = FileList;
@@ -76,10 +89,13 @@ namespace WPFFiler.ViewModels {
         }
 
         private DelegateCommand changeToTwoScreenModeCommand;
-        public DelegateCommand ChangeToTwoScreenModeCommand {
+        public DelegateCommand ChangeToTwoScreenModeCommand
+        {
             get => changeToTwoScreenModeCommand ?? (changeToTwoScreenModeCommand = new DelegateCommand(
-                () => {
-                    if(mainFileListStorage != null || subFileListStorage != null) {
+                () =>
+                {
+                    if (mainFileListStorage != null || subFileListStorage != null)
+                    {
                         FileList = mainFileListStorage;
                         FileList.BothViewBinding = false;
                         SubFileList = subFileListStorage;
